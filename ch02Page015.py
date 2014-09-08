fc2f209742c5c3687d6a1c857aea795da6b0541d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2014-09-08 18:52:16 +0200

    Create ch02Page015.py

---
+++ ch02Page015.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+babbage = turtle.Turtle()
+
+babbage.left(90)
+babbage.forward(100)
+babbage.right(90)
+babbage.circle(10)
+babbage.left(15)
+babbage.forward(50)
+babbage.left(157)
+window.exitonclick()
