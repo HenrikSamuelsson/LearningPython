b04eb70c478933aa19f0c2101dde6965b5509ac4
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2014-09-09 18:05:01 +0200

    Update ch02Page015.py

--- ch02Page015.py
+++ ch02Page015.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import turtle
 
 window = turtle.Screen()
@@ -7,7 +9,10 @@ babbage.left(90)
 babbage.forward(100)
 babbage.right(90)
 babbage.circle(10)
-babbage.left(15)
-babbage.forward(50)
-babbage.left(157)
+
+for i in range(1, 24):
+    babbage.left(15)
+    babbage.forward(50)
+    babbage.left(157)
+    babbage.forward(50)
 window.exitonclick()
